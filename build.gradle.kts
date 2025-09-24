534ae23a86447978666fbfda1b1d2a322a976172
sirezo <joebertcerezo@gmail.com>
2025-09-25 06:40:56 +0800

    chore: add build.gradle.kts for project configuration and dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  kotlin("plugin.jpa") version "1.9.25"
+
+  id("org.springframework.boot") version "3.5.6"
+  id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+  id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.cerezo"
+version = "0.0.1-SNAPSHOT"
+description = "Demo showing simple shed"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-mail")
+  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.flywaydb:flyway-core")
+  implementation("org.flywaydb:flyway-database-postgresql")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+  runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+  runtimeOnly("org.postgresql:postgresql")
+
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testImplementation("org.springframework.security:spring-security-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
+}
+
+allOpen {
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.MappedSuperclass")
+  annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
