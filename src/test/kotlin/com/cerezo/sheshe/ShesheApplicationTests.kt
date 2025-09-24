a2e52bc348f94f2039f3173e1ccd894ff99ab0bb
sirezo <joebertcerezo@gmail.com>
2025-09-25 06:41:49 +0800

    feat: add initial test class for Sheshe application context loading

---
+++ src/test/kotlin/com/cerezo/sheshe/ShesheApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.cerezo.sheshe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShesheApplicationTests {
+  @Test
+  fun contextLoads() {
+  }
+}
