7e92853c21ed82113722716cea0fa333b25244dd
sirezo <joebertcerezo@gmail.com>
2025-09-25 06:41:17 +0800

    feat: create ShesheApplication as the main entry point for the application

---
+++ src/main/kotlin/com/cerezo/sheshe/ShesheApplication.kt
@@ -0,0 +1,11 @@
+package com.cerezo.sheshe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShesheApplication
+
+fun main(args: Array<String>) {
+  runApplication<ShesheApplication>(*args)
+}
